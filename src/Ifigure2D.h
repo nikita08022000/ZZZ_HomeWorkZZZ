78cde7045526f2653fe3dc2e5e495557b23b76f5
Epstein_Sweat_Island <meleshkonikita08022010@mail.ru>
2026-04-07 21:01:29 +0300

    Update Ifigure2D.h
    Интерфейс 2-Д фигур

--- src/Ifigure2D.h
+++ src/Ifigure2D.h
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <cmath>
 
+const double PI = 3.1415;
+
+class I2DShape {
+public:
+    virtual ~I2DShape() {}
+    virtual double area() const = 0;
+    virtual double perimeter() const = 0;
+    virtual const char* name() const = 0;
+};
