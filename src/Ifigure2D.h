4b4e200c65d266c76bd1006ebcf6a138d6b91da8
Epstein_Sweat_Island <meleshkonikita08022010@mail.ru>
2026-04-07 21:18:36 +0300

    Update Ifigure2D.h

--- src/Ifigure2D.h
+++ src/Ifigure2D.h
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <cmath>
 
-const double PI = 3.1415;
-
-class I2DShape {
-public:
-    virtual ~I2DShape() {}
-    virtual double area() const = 0;
-    virtual double perimeter() const = 0;
-    virtual const char* name() const = 0;
-};
